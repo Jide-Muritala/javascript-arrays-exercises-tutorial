7040d5058e5c1ff00e58b6eca7f54a6ebf440ed8
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-05-25 15:35:43 +0000

    exercise 20.6

--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,11 @@
 let theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1];
 
 //your code here
+let wiki = theBools.map(myFunction)
+
+function myFunction(num) {
+    if (num === 0) return 'woko';
+    return 'wiki';
+}
+
+console.log(wiki)
